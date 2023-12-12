ba8fe135c1b367d9ee5f87b89809afd8709ec07e
EsauRangel <esaurangel11@gmail.com>
2023-12-12 00:14:10 -0600

    Se configuro la imagen con url temporal en appends

--- app/Models/TeacherTasks.php
+++ app/Models/TeacherTasks.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class TeacherTasks extends Model
 {
@@ -20,4 +21,18 @@ class TeacherTasks extends Model
         return $this->belongsTo(Student::class);
     }
 
+    protected $appends = ["secureURL"];
+
+    public function getSecureURLAttribute()
+    {
+        if (is_null($this->image_url)) {
+            return null;
+        }
+
+        if (Storage::disk('task-images')->exists($this->image_url)) {
+            return Storage::disk('task-images')->temporaryUrl($this->image_url, now()->addMinutes(30));
+        } else {
+            return null;
+        }
+    }
 }
